d766884fb53b7864d5b2bdc7b0cfbf400b72da9a
leemoby <leemoby.cn@gmail.com>
2017-05-05 18:08:46 +0800

    test 'index.html' page lagout

---
+++ js/base.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Moby on 2017/5/5.
+ */
+;(function () {
+    'use strict';
+    // console.info("$", $); // 测试jQuery是否加载成功
+    // console.info("jQuery", jQuery); // 测试jQuery是否加载成功
+
+})();
+/**
+ * (function(){ ... })()
+ * 写法用途：
+ * 1.自调用
+ * 2.限定作用域
+ */
